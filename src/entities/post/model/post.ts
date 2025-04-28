0364281e393e0bf8d5e21e6ce856dfd36c5a2933
HoJae Lee <hojae.lee@softly.ai>
2025-04-28 17:23:41 +0900

    refactor: separate tag type

--- src/entities/post/model/post.ts
+++ src/entities/post/model/post.ts
@@ -3,11 +3,13 @@ export type Reactions = {
   dislikes: number
 }
 
+export type Tag = string
+
 export type Post = {
   id: string
   title: string
   body: string
-  tags: string[]
+  tags: Tag[]
   reactions: Reactions
   views: number
   userId: string
