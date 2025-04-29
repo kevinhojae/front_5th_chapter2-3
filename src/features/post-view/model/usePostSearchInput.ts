c5374cfbc7e1db42bb4494eb31848d3f0e8d52ab
HoJae Lee <hojae.lee@softly.ai>
2025-04-29 19:44:25 +0900

    refactor: post search filter ui

---
+++ src/features/post-view/model/usePostSearchInput.ts
@@ -0,0 +1,43 @@
+import { useLocation, useNavigate } from "react-router-dom"
+import { fetchPostsBySearchQuery } from "../api/fetchPostsBySearchQuery"
+import { fetchPostsWithAuthor } from "../api/fetchPostsWithAuthor"
+import { usePagination } from "./PaginationProvider"
+import { usePosts } from "./PostProvider"
+import { usePostFilters } from "./FilterProvider"
+
+export const usePostSearchInput = () => {
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  const { setLoading, setPosts } = usePosts()
+  const { limit, skip, setTotal } = usePagination()
+  const { searchQuery, setSearchQuery } = usePostFilters()
+
+  const updateURL = () => {
+    const params = new URLSearchParams(location.search)
+    if (searchQuery) params.set("search", searchQuery)
+    navigate(`?${params.toString()}`)
+  }
+
+  const handlePostSearch = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      setLoading(true)
+
+      try {
+        const { posts, total } = searchQuery
+          ? await fetchPostsBySearchQuery(searchQuery)
+          : await fetchPostsWithAuthor(limit, skip)
+
+        setPosts(posts)
+        setTotal(total)
+        updateURL()
+      } catch (error) {
+        console.error("게시물 검색 오류:", error)
+      } finally {
+        setLoading(false)
+      }
+    }
+  }
+
+  return { handlePostSearch, searchQuery, setSearchQuery }
+}
---
+++ src/features/post-view/ui/PostSearchInput.tsx
@@ -0,0 +1,20 @@
+import { Search } from "lucide-react"
+import { Input } from "../../../shared/ui"
+import { usePostSearchInput } from "../model/usePostSearchInput"
+
+export function PostSearchInput() {
+  const { searchQuery, setSearchQuery, handlePostSearch } = usePostSearchInput()
+
+  return (
+    <div className="relative">
+      <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
+      <Input
+        placeholder="게시물 검색..."
+        className="pl-8"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        onKeyPress={(e) => e.key === "Enter" && handlePostSearch(e)}
+      />
+    </div>
+  )
+}
