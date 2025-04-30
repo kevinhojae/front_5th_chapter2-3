1d67542b7db1470049c5da2ea1e74c4908e945e6
HoJae Lee <hojae.lee@softly.ai>
2025-04-30 23:40:25 +0900

    feat: wrap app with queryclient

---
+++ src/app/App.tsx
@@ -0,0 +1,33 @@
+import { BrowserRouter as Router } from "react-router-dom"
+import Header from "../widgets/ui/Header.tsx"
+import Footer from "../widgets/ui/Footer.tsx"
+import PostsManagerPage from "../pages/PostsManagerPage.tsx"
+import { PostProvider } from "../features/post-view/model/PostContext.tsx"
+import { PaginationProvider } from "../features/post-view/model/PaginationContext.tsx"
+import { PostFilterProvider } from "../features/post-view/model/PostFilterContext.tsx"
+import { QueryClientProvider } from "@tanstack/react-query"
+import { queryClient } from "./lib/queryClient.ts"
+
+const App = () => {
+  return (
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow container mx-auto px-4 py-8">
+          <QueryClientProvider client={queryClient}>
+            <PaginationProvider>
+              <PostFilterProvider>
+                <PostProvider>
+                  <PostsManagerPage />
+                </PostProvider>
+              </PostFilterProvider>
+            </PaginationProvider>
+          </QueryClientProvider>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  )
+}
+
+export default App
