b0c172896419d5027c4fb12126abd1fa5b843d9e
HoJae Lee <hojae.lee@softly.ai>
2025-04-30 16:32:21 +0900

    feat: addPost api request function

---
+++ src/features/post-add/api/addPost.ts
@@ -0,0 +1,17 @@
+import { Post } from "../../../entities/post/model/post"
+import { PostWithAuthor } from "../../post-view/api/adaptPostWithAuthor"
+
+type AddPostDTO = Pick<Post, "title" | "body" | "userId">
+
+export const addPost = async (newPost: AddPostDTO): Promise<PostWithAuthor> => {
+  const response = await fetch("/api/posts/add", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(newPost),
+  })
+
+  const data = await response.json()
+
+  // TODO: error handling
+  return data
+}
