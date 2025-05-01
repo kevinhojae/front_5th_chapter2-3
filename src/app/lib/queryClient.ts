0037774cd0d65d6979aa88613c3cff6b6e58bea5
HoJae Lee <hojae.lee@softly.ai>
2025-05-01 20:11:13 +0900

    feat: global error handling logic for queryClient

--- src/app/lib/queryClient.ts
+++ src/app/lib/queryClient.ts
@@ -1,3 +1,10 @@
-import { QueryClient } from "@tanstack/react-query"
+import { QueryCache, QueryClient } from "@tanstack/react-query"
+import { toast } from "react-toastify"
 
-export const queryClient = new QueryClient()
+export const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error) => {
+      toast.error(error.message || "데이터를 불러오는데 실패했습니다")
+    },
+  }),
+})
