637224e1bd03b66d10650dd5f346bfe30fc832c9
HoJae Lee <hojae.lee@softly.ai>
2025-05-01 17:10:53 +0900

    refactor: capitalize primitive ui component file name

--- src/widgets/layout/TablePaginator.tsx
+++ src/widgets/layout/TablePaginator.tsx
@@ -1,6 +1,6 @@
 import { usePaginationParams } from "@shared/lib/hooks/usePaginationParams"
-import { Button } from "@shared/ui/button"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@shared/ui/select"
+
+import { Button, Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/shared/ui"
 
 export function TablePaginator() {
   return (
